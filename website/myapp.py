fbf8b72e68d24315ca53bc8a8e2bc47bf92ee81b
Ashton Balu <ab23223@students.sacredheart.school.nz>
2025-02-04 08:30:08 +0000

    hi

---
+++ website/myapp.py
@@ -0,0 +1,18 @@
+from bottle import run,route,template, static_file
+import requests
+
+@route('/static/<filepath:path>')
+def server_static(filepath):
+     return static_file(filepath, root='./static')
+
+
+@route('/')
+def index():
+     return template('homepage')
+
+@route('/index')
+def index():
+     return template('index')
+
+#main
+run(reloader=True)
