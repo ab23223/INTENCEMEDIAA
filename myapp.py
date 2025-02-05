f84e6790fcc64aa69d6df9661171a1741a31a1f5
Ashton Balu <ab23223@students.sacredheart.school.nz>
2025-02-05 02:15:37 +0000

    album added

--- myapp.py
+++ myapp.py
@@ -1,14 +1,26 @@
-from bottle import run,route,template, static_file
-import requests
+from bottle import run, route, template, static_file
 
+# Route to serve static files like images, CSS, etc.
 @route('/static/<filepath:path>')
 def server_static(filepath):
-     return static_file(filepath, root='./static')
+    return static_file(filepath, root='./static')
 
-
+# Route for home page (shows albums or general content)
 @route('/')
 def index():
-     return template('index')
+    return template('index')  # Make sure you have an 'index.tpl' template in the 'views/' folder
 
-#main
+# Route for 'gmshow' album page
+@route('/album_gmshow.html')
+def album_gmshow():
+    return template('album_gmshow.html')  # Make sure you have an 'album_gmshow.tpl' template in the 'views/' folder
+
+# Route for 'car' album page
+@route('/album_car.html')
+def album_car():
+    return template('album_car.html')  # Make sure you have an 'album_gmshow.tpl' template in the 'views/' folder
+
+
+
+# Run the app
 run(reloader=True)
